5ae1914e4a650d686320ddac5a81098dafa0ac2c
donggyun <donggyunkim12@gmail.com>
2025-03-03 14:29:25 +0900

    feat(app): 초기 프로젝트 세팅
    - 초기 폴더 구조 생성
    - emotion, mui, tanstack-query, axios, react-router-dom. zustand 의존성 설치

---
+++ src/routes/index.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import DefaultLayout from './layouts/Default';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+const router = createBrowserRouter([
+  {
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '*',
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
+
+export default function Router() {
+  return <RouterProvider router={router} />;
+}
---
+++ src/routes/pages/Home.tsx
@@ -0,0 +1,14 @@
+import { css } from '@emotion/react';
+
+const Home = () => {
+  return <div css={testbox}>test</div>;
+};
+
+const testbox = css`
+  background-color: black;
+  color: white;
+  width: 100px;
+  height: 100px;
+`;
+
+export default Home;

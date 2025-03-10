88b8ee519130692b9186b5c3619d2b2d6c2e4ed6
ph0109 <hyejeongp2@gmail.com>
2025-03-10 11:18:18 +0900

    feat(index): 경로 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,12 @@ import DefaultLayout from './layouts/Default';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import AdminLayout from './layouts/Admin';
+import LoginPage from './pages/ParticipantLoginPage';
+import RoleSelectionPage from './pages/RoleSelectionPage';
+import AdminLoginPage from './pages/AdminLoginPage';
+import SignupPage from './pages/SignupPage';
+import FindIdPage from './pages/FindIdPage';
+import ResetPasswordPage from './pages/ResetPasswordPage';
 import OnBoarding from './pages/OnBoarding';
 
 const router = createBrowserRouter([
@@ -12,6 +18,30 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Home />,
+      },
+	    {
+		    path: '/participant-login',
+		    element: <LoginPage />,
+	    },
+	    {
+		    path:'/rolesection',
+		    element: <RoleSelectionPage/>,
+	    },
+      {
+          path: '/admin-login',
+          element: <AdminLoginPage/>,
+      },
+      {
+        path: '/signup',
+        element: <SignupPage/>,
+      },
+      {
+        path: '/find-id',
+        element: <FindIdPage/>,
+      },
+      {
+        path: '/reset-password',
+        element: <ResetPasswordPage/>,
       },
       {
         path: '/onboarding',

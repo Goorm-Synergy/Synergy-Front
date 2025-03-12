86f6d2e8a5f9cade859ae992cc8d74e5440d4864
donggyun <donggyunkim12@gmail.com>
2025-03-12 16:15:28 +0900

    fix(onboarding): Info 퍼널 페이지 내부 스키마 내용 추가 및 스타일링 수정

--- src/utils/schemas/onboarding-schema.ts
+++ src/utils/schemas/onboarding-schema.ts
@@ -30,15 +30,37 @@ export const MoreInfoSchema = z.object({
   interested_list: InterestedSchema.shape.interested_list,
   work: WorkSchema.shape.work,
   info: z.object({
-    hope_job: z.string().min(1, '희망하는 직업을 선택해주세요.'),
+    hope_job: z.string().min(1, '희망하는 직무를 선택해주세요.'),
+    education: z.string().min(1, '학력을 선택해주세요.'),
+    age: z.string().min(1, '연령대를 선택해 주세요.'),
     skills: z.string().min(1, '보유한 기술을 입력해주세요.'),
-    experience: z.string().min(1, '경력을 입력해주세요.'),
-    others_experience: z.string().optional(),
+    experience: z.string().min(1, '경력을 선택해주세요.'),
+    hope_location: z.string().min(1, '희망하는 근무 지역을 선택해주세요.'),
+    profile_img: z
+      .any()
+      .refine((file: File) => !file.name, '증명 사진을 넣어주세요.')
+      .refine(
+        (file) => file.size < 5000000,
+        '사진의 사이즈는 5MB를 넘어갈 수 없습니다..',
+      )
+      .refine(
+        (file) => checkFileType(file),
+        '사진은 jpg, jepg, png 형식만 등록 가능합니다.',
+      ),
     cover_letter: z.string().min(10, '자기소개서를 10자 이상 입력해주세요.'),
-    location: z.string().optional(),
+    others_experience: z.string().optional(),
+    salary: z.string().optional(),
     company: z.string().optional(),
     culture: z.string().optional(),
     purpose: z.string().optional(),
-    salary: z.string().optional(),
   }),
 });
+
+function checkFileType(file: File) {
+  if (file?.name) {
+    const fileType = file.name.split('.').pop();
+    if (fileType === 'jpg' || fileType === 'png' || fileType === 'jpeg')
+      return true;
+  }
+  return false;
+}

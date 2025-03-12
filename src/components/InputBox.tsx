b3df78aee2d67471de431249c13464761cf1689a
donggyun <donggyunkim12@gmail.com>
2025-03-12 16:15:01 +0900

    style(onboarding): Interested, Work 퍼널 페이지 컴포넌트별 스타일링 수정

--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -1,28 +1,38 @@
-import { css, FormControl, Input, InputLabel } from '@mui/material';
+import { css, FormControl, Input, useTheme } from '@mui/material';
 
 interface Props {
   label: string;
   id: string;
-  isRequired: boolean;
+  isRequired?: boolean;
   placeholder?: string;
   onChange?: (value: string) => void;
 }
 
 const InputBox = ({ label, id, isRequired, placeholder, onChange }: Props) => {
+  const { palette, typo, radius } = useTheme();
+
   return (
-    <FormControl
-      required={isRequired}
-      css={css`
-        border: 1px solid #ddd;
-        padding: 10px;
-        border-radius: 10px;
-      `}
-    >
-      <InputLabel>{label}</InputLabel>
+    <FormControl required={isRequired}>
+      <span
+        css={css`
+          ${typo.sub.m}
+          color: ${palette.text.primary};
+          margin-bottom: 8px;
+        `}
+      >
+        {`${label} ${isRequired ? '*' : ''}`}
+      </span>
       <Input
+        disableUnderline
         id={id}
         placeholder={placeholder || ''}
         onChange={(e) => onChange?.(e.target.value)}
+        css={css`
+          border: 1px solid ${palette.border.secondary};
+          border-radius: ${radius.sm};
+          padding: 11px 20px 10px;
+          background-color: ${palette.background.quaternary};
+        `}
       />
     </FormControl>
   );

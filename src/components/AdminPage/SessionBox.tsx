99463b66b881697714ac0522d41050831fb886cf
ph0109 <hyejeongp2@gmail.com>
2025-03-20 11:21:45 +0900

    feat(qrPopup): qr 팝업 구현

--- src/components/AdminPage/SessionBox.tsx
+++ src/components/AdminPage/SessionBox.tsx
@@ -5,6 +5,7 @@ import QrCodeIcon from '@mui/icons-material/QrCode';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import ConfirmDeleteDialog from '../ConfirmDeleteDialog';
+import QRPopup from './Popup/QRPopup';
 
 interface SessionBoxProps {
   date: string;
@@ -20,7 +21,8 @@ interface SessionBoxProps {
 const SessionBox = ({ date, place, time, title, speaker, onDelete, onEdit }: SessionBoxProps) => {
   const theme = useTheme();
   const { palette, spacing, typo } = theme;
-  const [open, setOpen] = useState(false);
+  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
+  const [qrPopupOpen, setQrPopupOpen] = useState(false);
 
   const boxStyle = css`
     position: relative;
@@ -42,44 +44,53 @@ const SessionBox = ({ date, place, time, title, speaker, onDelete, onEdit }: Ses
   const iconContainerStyle = css`
     color: ${palette.icon.primary};
     position: absolute;
-    top: 8px;           
-    right: 8px;        
+    top: 8px;
+    right: 8px;
     display: flex;
-    gap: 2px;   
+    gap: 2px;
   `;
 
   return (
-    <Box css={boxStyle}>
-      <ConfirmDeleteDialog
-        open={open}
-        title="세션을 삭제하시겠습니까?"
-        description="세션 삭제 시 복구가 불가능합니다."
-        onClose={() => setOpen(false)}
-        onConfirm={() => {
-          onDelete();
-          setOpen(false);
-        }}
-      />
-      <Box css={iconContainerStyle}>
-        <IconButton size="small" color="inherit">
-          <QrCodeIcon fontSize="small" />
-        </IconButton>
-        <IconButton size="small" color="inherit" onClick={onEdit}>
-          <EditIcon fontSize="small" />
-        </IconButton>
-        <IconButton size="small" color="inherit" onClick={() => setOpen(true)}>
-          <DeleteIcon fontSize="small" />
-        </IconButton>
+    <>
+      <Box css={boxStyle}>
+        <ConfirmDeleteDialog
+          open={deleteDialogOpen}
+          title="세션을 삭제하시겠습니까?"
+          description="세션 삭제 시 복구가 불가능합니다."
+          onClose={() => setDeleteDialogOpen(false)}
+          onConfirm={() => {
+            onDelete();
+            setDeleteDialogOpen(false);
+          }}
+        />
+        <QRPopup
+          open={qrPopupOpen}
+          onClose={() => setQrPopupOpen(false)}
+          qrCodeLabel={title}
+          description={speaker}
+        />
+
+        <Box css={iconContainerStyle}>
+          <IconButton size="small" color="inherit" onClick={() => setQrPopupOpen(true)}>
+            <QrCodeIcon fontSize="small" />
+          </IconButton>
+          <IconButton size="small" color="inherit" onClick={onEdit}>
+            <EditIcon fontSize="small" />
+          </IconButton>
+          <IconButton size="small" color="inherit" onClick={() => setDeleteDialogOpen(true)}>
+            <DeleteIcon fontSize="small" />
+          </IconButton>
+        </Box>
+
+        {/* 정보 영역 */}
+        <Typography css={infoStyle}>{date} {place}</Typography>
+        <Typography css={infoStyle}>{time}</Typography>
+        <Typography css={infoStyle}>{title} {speaker}</Typography>
+
+        {/* 차트 데이터 영역 */}
+        <Typography variant="body2">!차트 데이터 표시 영역!</Typography>
       </Box>
-
-      {/* 정보 영역 */}
-      <Typography css={infoStyle}>{date} {place}</Typography>
-      <Typography css={infoStyle}>{time}</Typography>
-      <Typography css={infoStyle}>{title} {speaker}</Typography>
-
-      {/* 차트 데이터 영역 */}
-      <Typography variant="body2"> !차트 데이터 표시 영역!</Typography>
-    </Box>
+    </>
   );
 };
 

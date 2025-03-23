f00dcb5cd24d27e2f2dc22e719e00922b5f49eb8
donggyun <donggyunkim12@gmail.com>
2025-03-23 14:58:25 +0900

    feat(mypage): 포인트 적립 시스템 모달 구현

---
+++ src/constant/point-system.ts
@@ -0,0 +1,57 @@
+export const POINT_SYSTEM = {
+  BRONZE: {
+    image:
+      'https://s3-alpha-sig.figma.com/img/b5be/dd12/93985b80f543c2cec6df696fd0b4c70a?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=PbK5kqoWAYd4b4Df3MVzI8WsVbJpW0fUJVQoifTvHJ1Bp7INgmrIVCXxgdgfjVSUw42KTVKOqpfC1uzI9vah7igVin0hJUG6aA7PzA8mwKBj~JcI-Nnc~0HemXIimgTRe4oIv4lERzQxFx1iQdu9gXAj3zTNrp0~bLR-sNzc~oFw1frZDxONGQX2U0iATXQHZcakNuo0j2B8KhRVDvJj~RJ7BDIIV-SzE4sSQk5DEM0h7HthnUe~piSrHww~gRCPoS3w9SsoD9i0Viv0MGYGXACo5Rb7MeslDPPNruJjRq3vDaesAaYv8Bj3Gf1fnq6Zj-s0TzBNyRSzcHZ8N6VNHw__',
+    desc: '100P-299P',
+    content: [
+      '세션 자료 다운로드',
+      '컨퍼런스 내 카페 커피 쿠폰 1매',
+      '온라인 이력서 첨삭 서비스 1회*',
+      '채용 담당자와 온라인 커리어 상담 30분*',
+    ],
+  },
+  SILVER: {
+    image:
+      'https://s3-alpha-sig.figma.com/img/a355/56e2/ffd3228d1b986326e753e3796248114e?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=KC0Wrrt8K9BxxOmnT~nK8hn49-7-G0JGj~se1-GQlLC77EMAI5-5DmGwdpAQZNYfs~40hWU7GSh7AirPeR0apgIITDfd2IbS3ZTzVASZmwW1~7iGR9zcHRwK3-h5lmnhh1zbg0ONIaljw7VWWQ3gOPoTFC3Uo~6sDBiBd0yTBrYfAACtpRhuRYhd-Ni0-lMKslpE5s4082IlqL-5IqfsHTHTdSD3a-PE0JIrtv2n4lpHlOuNUNRpUlwUbeojsMQB-qNAOfYkvyI9bZHP~pBvCTumivpQNS53m8amIbqN6dlo0dn5podYS0mCHl5512fHzyMfZ-A2xJsXRY~S~fhAhw__',
+    desc: '300P-799P',
+    content: [
+      '+ BRONZE 혜택',
+      '다음 IT 컨퍼런스 티켓 10% 할인',
+      '네트워킹 라운지 입장권 제공',
+      '파트너 기업 온라인 채용 설명회 티켓 1매*',
+    ],
+  },
+  GOLD: {
+    image:
+      'https://s3-alpha-sig.figma.com/img/aa83/187e/f1c32373d2e23f79312a6cf23afd5994?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=uJ3NJXlEKl-Lj2oWiI0lOMy1cyFfTLMEDB39BubD8gtS2mNZB710IdqZc2DMW~QZcZiue5HLO6n3lQHIk-auU8uvUPBxlS0UDzPiDgV4CTon19cTwd6E~cBLTamabD~9Rb9rh6h61-h-aQ6bkhLIEi63yj2zj5UwuenDgptQx2~J6ANytgk44LXxF-xvnMakxJ9g69QzQ7BWn~PqP3NE~uI5xOO7d2CXyfE3uJstMXUZfAfHEPlnNf~yGO8mePHVEUMwMx7xoilEXDs5Rtm1ykOuPKI~1jFHFl1PjYG5CZFBo2EwuutnYAYJweUaEJvD8LlzFxgXkB9DLz6BsZVWqQ__',
+    desc: '800P-1,299P',
+    content: [
+      '+ SILVER 혜택',
+      '굿즈 세트(한정판 노트북 스티커, 텀블러 등) 제공',
+      '파트너 기업 인턴십 지원 시 서류 전형 가산점*',
+      'IT 업계 전문가와의 1시간 1:1 온라인 멘토링 세션*',
+    ],
+  },
+  PLATINUM: {
+    image:
+      'https://s3-alpha-sig.figma.com/img/ca10/9860/53c5594e00742d992f4a8284255a17f9?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=gh9dwSpBsJN6DRUKqIY2XiW35NPkj~8f-TNZSeYvnlOeq0y5R9sM~7dQAbswtQrlBLyNlS1uFrq6u5dGYJQ14H0qmzbEjHYRGftQu8T5rsiG~wT863y9VnS-IHanNOJhsqTmCceruN-Hd9wEnufkuJcSTnJRbv96wyr~JYTIg0KP0F8uFPGf3-WZNC1TH3u6UQ5fVlkSPBN4QF0FBmmv1hlKgIM-kgKrPoJcLY9zQfR6oqXv9Nbh9kGVl~bJ~c0d18-~Xyz2c1wczOWuxCk2n16oV96-tDxYfdqglmAj83Ko9Gi7yak19yfdG-7agnxB77T6usC-~nqfjfYgJIrnFA__',
+    desc: 'VIP 전용',
+    content: [
+      '+ GOLD 혜택',
+      'VIP 네트워킹 이벤트 초대장 1매 증정',
+      '다음 컨퍼런스 전일권 1매 증정',
+      '굿즈 1세트, 커피 쿠폰 1매 추가 제공',
+      '프리미엄 세션 참여권 제공',
+    ],
+  },
+};
+
+export const POINT_EARN = [
+  { text: '최초 회원가입', point: '50P' },
+  { text: '세션 참여 QR 코드 체크인', point: '30P' },
+  { text: '세션 Q&A 참여', point: '50P' },
+  { text: '부스 방문 QR 코드 체크인', point: '20P' },
+  { text: '설문 조사 참여', point: '30P' },
+  { text: '컨텐츠 공유', point: '20P' },
+  { text: '채용 담당자 미팅', point: '50P' },
+];

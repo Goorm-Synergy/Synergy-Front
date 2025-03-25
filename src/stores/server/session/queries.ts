a5ef0f1afccd65ccdbb9cf71b239078db87595c2
donggyun <donggyunkim12@gmail.com>
2025-03-25 22:49:13 +0900

    feat: 세션 리스트 조회 api 연동

---
+++ src/stores/server/session/queries.ts
@@ -0,0 +1,18 @@
+import { fetchSessionDetail, fetchSessionList } from '@api/session-controller';
+import { queryOptions } from '@tanstack/react-query';
+
+export const sessionQueries = {
+  all: () => ['session'],
+  list: () => [...sessionQueries.all(), 'list'],
+  lists: () =>
+    queryOptions({
+      queryKey: [...sessionQueries.list()],
+      queryFn: () => fetchSessionList(),
+    }),
+  detail: (sessionId: number) => [...sessionQueries.all(), 'detail', sessionId],
+  details: (sessionId: number) =>
+    queryOptions({
+      queryKey: [...sessionQueries.detail(sessionId)],
+      queryFn: () => fetchSessionDetail(sessionId),
+    }),
+};

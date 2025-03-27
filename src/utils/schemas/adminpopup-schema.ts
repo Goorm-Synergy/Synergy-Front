e54c6d77a5f76ae2913d31062e26935ee7cce8ab
ph0109 <hyejeongp2@gmail.com>
2025-03-27 09:31:32 +0900

    fix(schema): 날짜 스키마 변경

--- src/utils/schemas/adminpopup-schema.ts
+++ src/utils/schemas/adminpopup-schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 // 정규식 패턴
-const dateRegex = /^\d{4}\.\d{2}\.\d{2}$/; // YYYY.MM.DD
+const dateRegex = /^\d{4}\-\d{2}\-\d{2}$/; // YYYY-MM-DD
 const timeRegex = /^([01]\d|2[0-3]):([0-5]\d)$/; // 24시간제 00:00 ~ 23:59
 
 const isFutureDate = (dateString: string) => {
@@ -12,18 +12,17 @@ const isFutureDate = (dateString: string) => {
 
 // 세션 등록 스키마
 export const sessionSchema = z.object({
-  title: z.string().min(1, '세션 제목을 입력해 주세요.').max(30, '제목은 30자 이내로 입력해 주세요.'),
-  speaker: z.string().min(1, '발표자 성함을 입력해 주세요.'),
-  speakerPosition: z.string().min(1, '발표자의 직책을 입력해 주세요.').max(15, '직책은 15자 이내로 입력해 주세요.'),
-  progressDate: z.string()
-    .regex(dateRegex, '진행일은 YYYY-MM-DD 형식으로 입력해 주세요.')
-    .refine(isFutureDate, '진행일은 미래 날짜여야 합니다.'),
-  startTime: z.string().regex(timeRegex, '시작 시간은 24시간제 HH:MM 형식으로 입력해 주세요.'),
-  endTime: z.string().regex(timeRegex, '종료 시간은 24시간제 HH:MM 형식으로 입력해 주세요.'),
-  description: z.string().min(1, '세션 설명을 입력해 주세요.').max(150, '세션 설명은 150자 이내로 입력해 주세요.'),
-  multipartFile: z.any().optional(),
-  maximum: z.enum(['150', '200', '250'], { required_error: '최대 인원 수용을 선택해 주세요.' }),
-  domainAddress: z.string().min(1, '도메인 주소를 입력해 주세요.').optional(),
+    title: z.string().min(1, '세션 제목을 입력해 주세요.').max(30, '제목은 30자 이내로 입력해 주세요.'),
+    presenter: z.string().min(1, '발표자 성함을 입력해 주세요.'),
+    presenterRole: z.string().min(1, '발표자의 직책을 입력해 주세요.').max(15, '직책은 15자 이내로 입력해 주세요.'),
+    date: z.string()
+      .regex(dateRegex, '진행일은 YYYY-MM-DD 형식으로 입력해 주세요.')
+      .refine(isFutureDate, '진행일은 미래 날짜여야 합니다.'),
+    startTime: z.string().regex(timeRegex, '시작 시간은 24시간제 HH:MM 형식으로 입력해 주세요.'),
+    endTime: z.string().regex(timeRegex, '종료 시간은 24시간제 HH:MM 형식으로 입력해 주세요.'),
+    sessionDescription: z.string().min(1, '세션 설명을 입력해 주세요.').max(150, '세션 설명은 150자 이내로 입력해 주세요.'),
+    imageFile: z.any().optional(),
+    maxCapacity: z.enum(['150', '200', '250'], { required_error: '최대 인원 수용을 선택해 주세요.' }),
 });
 
 
@@ -41,12 +40,12 @@ export const boothSchema = z.object({
     name: z.string().max(30, { message: '컨퍼런스 명은 최대 30자까지 입력 가능합니다.' }),
     host: z.string().max(10, { message: '컨퍼런스 주최자는 최대 10자까지 입력 가능합니다.' }),
     startDate: z.string()
-      .regex(dateRegex, { message: '시작일은 YYYY.MM.DD 형식으로 입력해 주세요.' })
+      .regex(dateRegex, { message: '시작일은 YYYY-MM-DD 형식으로 입력해 주세요.' })
       .refine(isFutureDate, { message: '시작일은 오늘 이후의 날짜만 입력 가능합니다.' }),
     startTime: z.string()
       .regex(timeRegex, { message: '시작 시간은 24시간제로 00:00 형식으로 입력해 주세요.' }),
     endDate: z.string()
-      .regex(dateRegex, { message: '종료일은 YYYY.MM.DD 형식으로 입력해 주세요.' })
+      .regex(dateRegex, { message: '종료일은 YYYY-MM-DD 형식으로 입력해 주세요.' })
       .refine(isFutureDate, { message: '종료일은 오늘 이후의 날짜만 입력 가능합니다.' }),
     endTime: z.string()
       .regex(timeRegex, { message: '종료 시간은 24시간제로 00:00 형식으로 입력해 주세요.' }),
